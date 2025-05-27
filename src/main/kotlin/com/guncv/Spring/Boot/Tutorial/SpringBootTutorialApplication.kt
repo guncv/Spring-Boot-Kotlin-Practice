30320a548d9b8a2cf7dcb47d68056425deebf160
Chanagun Viriyasathapornpong <chanagun.vir@gmail.com>
2025-05-27 18:24:23 +0700

    set up  Kotlin project

---
+++ src/main/kotlin/com/guncv/Spring/Boot/Tutorial/SpringBootTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.guncv.Spring.Boot.Tutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootTutorialApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/guncv/Spring/Boot/Tutorial/SpringBootTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.guncv.Spring.Boot.Tutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
